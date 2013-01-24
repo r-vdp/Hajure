7502680fb08079c4b8014e720925d6a6c2a6544f
RamsesDeNorre <ramses.rommel@gmail.com>
2013-01-24 11:26:37 +0100

    Pretty print the AST.

--- Hajure.hs
+++ Hajure.hs
@@ -12,8 +12,12 @@ import Hajure.Parsing
 main :: IO ()
 main = do
   (x:_) <- getArgs
-  withFile x ReadMode (\h -> either print print . parse =<< hGetContents h)
+  withFile x ReadMode (\h -> either print printSExpr . parse =<< hGetContents h)
 
 parse :: String -> Either ParseError (Element String)
 parse = fmap listify . parseHajure
+
+printSExpr :: Show a => Element a -> IO ()
+printSExpr (Nested sexpr) = print sexpr
+printSExpr e              = print e
 
--- Hajure/Data.hs
+++ Hajure/Data.hs
@@ -1,8 +1,7 @@
 
 module Hajure.Data where
 
-import Control.Monad
-import Data.List
+import Data.List (intersperse)
 
 data Element a = Nested (SExpr a)
                | Ident  a
@@ -14,7 +13,25 @@ data Element a = Nested (SExpr a)
 newtype SExpr a = SExpr { unwrap :: [Element a] }
 
 instance Show a => Show (SExpr a) where
-  show (SExpr xs) = "S( " ++ showElements xs ++ " )"
-    where showElements = join . intersperse " " . embrace
-          embrace      = map (\x -> "{" ++ show x ++ "}")
+  show = unlines . showExpr "" ""
+    where
+      
+      chld = "|-- "
+      nxt  = "|   "
 
+      showExpr :: Show a => String -> String -> SExpr a -> [String]
+      showExpr acc1 acc2 e = start : body e ++ end
+        where start = acc1 ++ "S("
+              body  = map (showElem acc2) . unwrap
+              end   = [acc2 ++ ")"]
+
+      showElem :: Show a => String -> Element a -> String
+      showElem acc (Ident a)  = acc ++ chld ++ show a
+      showElem acc (Num a)    = acc ++ chld ++ show a
+      showElem acc (Op a)     = acc ++ chld ++ show a
+      showElem acc (List as)  = acc ++ chld ++ show as
+      showElem acc (Nested e) = unlines' . showExpr (acc ++ chld) (acc ++ nxt) $ e
+                                
+      unlines' :: [String] -> String
+      unlines' = concat . intersperse "\n"
+ 
