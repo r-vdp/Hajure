541125506b42ca21e1d747bcfa2c994aff439caa
RamsesDeNorre <ramses.rommel@gmail.com>
2013-01-24 12:25:46 +0100

    Improve pretty printing code.

--- Hajure/Data.hs
+++ Hajure/Data.hs
@@ -1,7 +1,8 @@
 
-module Hajure.Data where
+module Hajure.Data (Element(..), SExpr(..)) where
 
-import Data.List (intersperse)
+import Control.Applicative (liftA2)
+import Data.List (intercalate)
 
 data Element a = Nested (SExpr a)
                | Ident  a
@@ -14,24 +15,34 @@ newtype SExpr a = SExpr { unwrap :: [Element a] }
 
 instance Show a => Show (SExpr a) where
   show = unlines . showExpr "" ""
-    where
-      
-      chld = "|-- "
-      nxt  = "|   "
 
-      showExpr :: Show a => String -> String -> SExpr a -> [String]
-      showExpr acc1 acc2 e = start : body e ++ end
-        where start = acc1 ++ "S("
-              body  = map (showElem acc2) . unwrap
-              end   = [acc2 ++ ")"]
+chld :: String
+chld = "|-- "
 
-      showElem :: Show a => String -> Element a -> String
-      showElem acc (Ident a)  = acc ++ chld ++ show a
-      showElem acc (Num a)    = acc ++ chld ++ show a
-      showElem acc (Op a)     = acc ++ chld ++ show a
-      showElem acc (List as)  = acc ++ chld ++ show as
-      showElem acc (Nested e) = unlines' . showExpr (acc ++ chld) (acc ++ nxt) $ e
-                                
-      unlines' :: [String] -> String
-      unlines' = concat . intersperse "\n"
- 
+nxt :: String
+nxt  = "|   "
+
+withChld :: String -> String
+withChld = (++ chld)
+
+withNxt :: String -> String
+withNxt  = (++ nxt)
+
+showExpr :: Show a => String -> String -> SExpr a -> [String]
+showExpr acc1 acc2 e = start : body e ++ end
+  where start = acc1 ++ "S("
+        body  = map (showElem acc2) . unwrap
+        end   = [acc2 ++ ")"]
+
+showElem :: Show a => String -> Element a -> String
+showElem acc (Ident a)  = showAsChild acc a
+showElem acc (Num a)    = showAsChild acc a
+showElem acc (Op a)     = showAsChild acc a
+showElem acc (List as)  = showAsChild acc as
+showElem acc (Nested e) = unlines' . show' acc $ e
+  where show'    = liftA2 showExpr withChld withNxt
+        unlines' = intercalate "\n"
+
+showAsChild :: Show a => String -> a -> String
+showAsChild acc a = withChld acc ++ show a
+
