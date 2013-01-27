c2c36431c04e51afa20cfd5ab6e2cf8bccd50849
RamsesDeNorre <ramses.rommel@gmail.com>
2013-01-27 17:43:41 +0100

    Fix pretty print algorithm, nested s-expr in lists are now properly shown.

--- Hajure/Data.hs
+++ Hajure/Data.hs
@@ -4,6 +4,8 @@ module Hajure.Data
   , SExpr(..)
   ) where
 
+import Prelude hiding (showList)
+
 import Control.Applicative (liftA2)
 import Control.Monad (join)
 import Data.List (intercalate)
@@ -39,12 +41,13 @@ showExpr acc1 acc2 e = start : body e ++ end
         body  = map (showElem acc2) . unwrap
         end   = [acc2 ++ ")"]
 
-showExpr' :: String -> SExpr -> [String]
-showExpr' = liftA2 showExpr withChld withNxt
+showExpr' :: String -> SExpr -> String
+showExpr' acc  = unlines' . lifted acc
+  where lifted = liftA2 showExpr withChld withNxt
 
 showElem :: String -> Element -> String
-showElem acc (List es)  = showAsChild acc es
-showElem acc (Nested e) = unlines' . showExpr' acc $ e
+showElem acc (List es)  = showList    acc es
+showElem acc (Nested e) = showExpr'   acc e
 showElem acc e          = showAsChild acc e
 
 unlines' :: [String] -> String
@@ -52,4 +55,14 @@ unlines' = intercalate "\n"
 
 showAsChild :: Show a => String -> a -> String
 showAsChild acc a = withChld acc ++ show a
+
+showList :: String -> [Element] -> String
+showList acc = unlines' . showListElems (acc ++ chld) (acc ++ nxt)
+
+showListElems :: String -> String -> [Element] -> [String]
+showListElems acc1 acc2 es = start : showElems es ++ end
+  where showElems = map (showElem acc2)
+        start     = acc1 ++ "["
+        end       = [acc2 ++ "]"]
+
 
--- test.cl
+++ test.cl
@@ -2,15 +2,16 @@
   (list 4 5) 
   test 
   [1 2 3] 
-  (fun 
+  (fun_ 
     2 
     3.4
-    (nest + 2 (list 3 4) (list' 1 2))
+    (_ne_st + 2 (list 3 4) (list' 1 2))
   )
-  (r 4 (+ 5 6) test)
+  (rµ 4 (+ 5 6) te3st)
   -8.564
   (- 4 2)
   5.6e-2
   -5.7e-2
+  [ expr_inList (+ 3 4) (list 4 5) [1 2 3]]
 )
 
