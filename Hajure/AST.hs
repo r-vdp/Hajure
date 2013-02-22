429a2f8df7c9596cd39bc30073c54d5cfb1557bb
RamsesDeNorre <ramses.rommel@gmail.com>
2013-02-22 02:02:31 +0100

    Make one AST class for operations that can be done on AST nodes.

--- Hajure/AST.hs
+++ Hajure/AST.hs
@@ -26,42 +26,35 @@ import Hajure.Data
 -- >>> print n'
 -- List [Num 3.0,Num 4.0]
 
-class Listifiable a where
+class AST a where
   listify :: a -> Element
+  funify  :: a -> Element
 
-instance Listifiable Element where
+instance AST Element where
   listify (Nested s) = listify s
   listify (List xs)  = List (map listify xs)
   listify e          = e
 
-instance Listifiable SExpr where
+  funify (Nested s) = funify s
+  funify (List xs)  = List (map funify xs)
+  funify e          = e
+
+instance AST SExpr where
   listify s@(sexprView -> (x:xs))
     | isList x  = List (map listify xs)
     | otherwise = Nested (listify <$> s)
   listify s     = Nested s
+
+  funify s
+    | Just f <- toDefun s = f
+    | otherwise           = Nested (funify <$> s)
+
 
 isList :: Element -> Bool
 isList x
   | Ident i <- x
   , i == "list"  = True
   | otherwise    = False
-
--- Walk the AST with a State monad to gather all functions and then walk it again to substitute them??
--- what about scopes and functions which reference other functions? (Fix points needed!)
-
-
-class Funifiable a where
-  funify :: a -> Element
-
-instance Funifiable Element where
-  funify (Nested s) = funify s
-  funify (List xs)  = List (map funify xs)
-  funify e          = e
-
-instance Funifiable SExpr where
-  funify s
-    | Just f <- toDefun s = f
-    | otherwise           = Nested (funify <$> s)
 
 toDefun :: SExpr -> Maybe Element
 toDefun s
